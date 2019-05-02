e5d7fc5164c924b0b0f030f44503ba5a7848e590
shashank <uyshashank@gmail.com>
2019-05-02 14:39:43 +0530

    Alerts for empty field added

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,21 +1,32 @@
 let data;
+
 function save() {
     event.preventDefault();
-    data = {
-        "Name": $('#name').val(),
-        "Mobile": $('#mobile').val(),
-        "Category": $('#category').val()
+    if ($('#name').val() == '') {
+        $('.warning-name').show();
+    } else if ($('#mobile').val() == '') {
+        $('.warning-mob').show();
+    } else if ($('#name').val() && $('#mobile').val() == '') {
+        $('.warning-mob').show();
+        $('.warning-name').show();
+    } else {
+        data = {
+            "Name": $('#name').val(),
+            "Mobile": $('#mobile').val(),
+            "Category": $('#category').val()
+        }
+
+        fetch('/save', {
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(data), // data can be `string` or {object}!
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.text())
+            .then(_response => {
+                let sr = $('.tbrow').length + 1;
+                $('#tableBody').append('<tr class="tbrow"><td scope="row">' + sr + '</td><td>' + data.Name + '</td><td>' + data.Mobile + '</td><td>' + data.Category + '</td>' + '<td class="text-center"> <a href="/edit"><i class="fas fa-pencil-alt"></i></a> </td> <td class="text-center"> <a href="/remove"><i class="far fa-trash-alt"></i></a> </td>' + '</tr>');
+            });
     }
 
-    fetch('/save', {
-            method: 'POST', // or 'PUT'
-            body: JSON.stringify(data), // data can be `string` or {object}!
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => res.text())
-        .then(_response => {
-            let sr = $('.tbrow').length + 1;            
-            $('#tableBody').append('<tr class="tbrow"><td scope="row">' + sr + '</td><td>' + data.Name + '</td><td>' + data.Mobile + '</td><td>' + data.Category + '</td></tr>');
-        });
 }
--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-let x = 500;
-function show(){        
-    $('.form').show(x);
-    $('#addBTN').hide(x);
-}
-function hide(){        
-    $('.form').hide(x);
-    $('#addBTN').show(x);
-}
