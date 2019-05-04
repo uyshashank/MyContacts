50ef3f6dabde0abf91795eec6be6570128f4530d
shashank <uyshashank@gmail.com>
2019-05-04 09:52:58 +0530

    Port Changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Custom modules
 const homepage = require('./controllers/homepage');
